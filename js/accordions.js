148b6c0220f681a4b847f1877cef02ea1d556a71
Axel <Kingstona@bk.ru>
2022-08-30 17:19:40 +0300

    project

---
+++ js/accordions.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelectorAll('.accordion-item-title').forEach(function(itemPlus) {
+        itemPlus.addEventListener('click', function(plusClick) {
+            const path = plusClick.currentTarget.dataset.path
+            const nameClass = plusClick.currentTarget.className
+            const activeBlock = document.querySelector(`[data-target="${path}"]`)
+            if ( nameClass === 'accordion-item-title accordion-item-title-active') {
+                document.querySelectorAll('.accordion-item-description').forEach(function(active) {
+                    active.classList.remove('accordion-item-description-active')
+                    active.style.height = 0 + 'px'
+                })
+                itemPlus.classList.remove('accordion-item-title-active')
+            } 
+            else {
+                document.querySelectorAll('.accordion-item-description').forEach(function(active) {
+                    active.classList.remove('accordion-item-description-active')
+                    active.style.height = 0 + 'px'
+                })
+                document.querySelectorAll('.accordion-item-title').forEach(function(deleteActive) {
+                    deleteActive.classList.remove('accordion-item-title-active')
+                }) 
+                itemPlus.classList.add('accordion-item-title-active')
+                activeBlock.style.height = activeBlock.scrollHeight + 'px'
+                activeBlock.classList.add('accordion-item-description-active') 
+            }  
+        })  
+    })
+})
---
+++ js/slider.js
@@ -0,0 +1,47 @@
+    window.onload = function() {
+        let listBlock = document.querySelector('.slider-list')
+        let itemSlider = document.querySelectorAll('.slider-item')
+        listBlock.style.height = itemSlider[0].scrollHeight + 'px'
+    };
+    document.addEventListener('DOMContentLoaded', function(){
+        let itemSlider = document.querySelectorAll('.slider-item')
+        let listBlock = document.querySelector('.slider-list')
+        let arrowLeft = document.querySelector('.slider_arrow-left')
+        let arrowRight = document.querySelector('.slider_arrow-right')
+        let sliderCount = document.querySelector('.slider-count')
+        let count = 0
+        function leftMove() {
+            count = count - 1
+            for(i = 0; i < itemSlider.length; i++) {
+                if (count === -1) {
+                    count = itemSlider.length - 1
+                }
+                itemSlider[i].classList.remove('slider-item-active');
+            }
+            listBlock.style.height = 0 + 'px'
+            listBlock.style.height = itemSlider[count].scrollHeight + 'px'
+            itemSlider[count].classList.add('slider-item-active');
+            sliderCount.textContent = `${count + 1} / ${itemSlider.length}`
+        }
+        function rightMove() {
+            
+            count = count + 1
+            
+            for(i = 0; i < itemSlider.length; i++) {
+                
+                if (itemSlider.length === count) {
+                    count = 0
+                }
+                itemSlider[i].classList.remove('slider-item-active');
+                
+            } 
+            listBlock.style.height = 0 + 'px'
+            listBlock.style.height = itemSlider[count].scrollHeight + 'px'
+            itemSlider[count].classList.add('slider-item-active');
+            sliderCount.textContent = `${count + 1} / ${itemSlider.length}`
+        }
+        
+        sliderCount.textContent = `${count + 1} / ${itemSlider.length}`
+        arrowLeft.addEventListener('click', leftMove)
+        arrowRight.addEventListener('click', rightMove)
+});
